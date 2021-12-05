91c338f469bef531d718efccc9488b63b8e464f1
Jaleel-VS <jeremikress@gmail.com>
2021-12-05 15:36:09 +0200

    add quote generator

---
+++ cogs/quote_generator.py
@@ -0,0 +1,84 @@
+"""
+Fun utility that creates quotes using the message content and username of a user
+and converts it to an image using html and css
+"""
+
+from discord.ext.commands import Cog, command, cooldown, BucketType
+from discord import Embed, File
+from cogs.utils.quote_generator_data.image_creator import create_image
+
+
+def get_img_url(user_id: int, url_identifier: str):
+    return f"https://cdn.discordapp.com/avatars/{user_id}/{url_identifier}.png?size=256"
+
+
+def create_image_embed(url):
+    file = File(url, filename="picture.jpg")
+    embed = Embed()
+    embed.set_image(url="attachment://picture.jpg")
+
+    return file, embed
+
+
+class QuoteGenerator(Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @command()
+    @cooldown(1, 10, type=BucketType.user)
+    async def quote(self, ctx, *user_input):
+        """
+        Generates a dramatically themed quote using a message url or your own message. Copy the url from a message you want quoted
+        and paste next to the command. Images and custom images won't show up and there's a limit to 150 words.
+
+        Example usage:
+        `$quote https://discord.com/channels/731403448502845501/808679873837137940/916938526329798718`
+        (creates a quote using a specific message)
+        `$quote ¡Viva México, cabrones!`
+        (creates a quote using your own message)
+
+        NOTE: The message id doesn't work, only the full link.
+
+        I'll make it a slash command eventually, should be easier to use
+        """
+        if len(user_input) == 0:
+            return await ctx.send("Please see type `$help quote` for info on correct usage")
+
+        if len(user_input) == 1 and len(user_input[0]) == 18 and user_input[0].isdigit():
+            return await ctx.send("You tried to use a message_id. Please use a link or just a regular message. See `$help quote` for correct usage")
+        user_nick = ""
+        user_avatar = ""
+        message_content = ""
+        if len(user_input) == 1 and user_input[0].startswith("https://discord.com/channels/"):
+            link = user_input[0].split('/')
+            server_id = int(link[4])
+            channel_id = int(link[5])
+            msg_id = int(link[6])
+
+            server = self.bot.get_guild(server_id)
+            if server is None:
+                return await ctx.send("I can't access this server")
+            channel = server.get_channel(channel_id)
+            if channel is None:
+                return await ctx.send("I can't access this channel")
+            message = await channel.fetch_message(msg_id)
+            user_id = message.author.id
+            user = await server.fetch_member(user_id)
+            message_content = message.content
+            user_nick = user.display_name if user.nick is None else user.nick
+            user_avatar = get_img_url(user_id, user.avatar)
+        else:
+            message_content = ' '.join(user_input)
+            user_nick = ctx.author.display_name if ctx.author.nick is None else ctx.author.nick
+            user_avatar = get_img_url(ctx.author.id, ctx.author.avatar)
+
+        if len(message_content) > 150:
+            return ctx.send("Beep boop, I can't create an image with that much text. I'm limited at 150 characters")
+        generated_url = create_image(user_nick, user_avatar, message_content)
+        # file_embed = create_image_embed(generated_url)
+
+        await ctx.send(file=File(generated_url))
+
+
+def setup(bot):
+    bot.add_cog(QuoteGenerator(bot))
---
+++ cogs/utils/quote_generator_data/image_creator.py
@@ -0,0 +1,83 @@
+from os import path
+from htmlwebshot import WebShot
+
+dir_path = path.dirname(path.dirname(path.realpath(__file__)))
+
+
+def create_image(user_name, user_avatar, message_content):
+    shot = WebShot()
+    shot.quality = 100
+    shot.params = {"--crop-h": 266, "--crop-w": 637, "--encoding": "utf-8"}
+    font_size = ""
+
+    font_sizes = {
+        60: 'x-large',
+        100: 'large',
+        150: 'small',
+    }
+
+    for fs in font_sizes.keys():
+        if len(message_content) <= fs:
+            font_size = font_sizes[fs]
+            break
+
+    html = f'''
+            <img src="{user_avatar}" class="myImage"/>
+            <div class="quote">
+                <p class="main-quote">{message_content}</p>
+                <p class="author"><span> {user_name} </span></p>
+            </div>
+    '''
+    css = f'''
+    .myImage {{
+        float: left;
+        -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+        filter: grayscale(100%);
+        border: 2px solid white;
+        border-right: 0;
+      }}
+
+
+      .quote {{
+          float: left;
+          width: 376px;
+          height: 256px;
+          margin-top: 0;
+          margin-bottom: 0;
+          background-color: #0c0c0c;
+          border: 2px solid white;
+          border-left: 0;
+
+          text-align: center;
+          }}
+
+      .main-quote {{
+        color: antiquewhite;
+        font-family: sans-serif;
+        font-size: {font_size};
+        font-style: italic;
+        padding: 10% 5% 5%;
+      }}
+
+      .author {{
+          color: antiquewhite;
+          font-size: larger;
+          font-family: cursive;
+      }}
+
+      span:after,
+    span:before{{
+        content:"\\00a0\\00a0\\00a0\\00a0\\00a0";
+        text-decoration:line-through;
+    }}
+
+    body {{
+        padding: 0;
+        margin: 0;
+    }}
+
+    '''
+
+    img_path = f"{dir_path}/quote_generator_data/picture.png"
+    shot.create_pic(html=html, css=css, output=img_path)
+    return img_path
