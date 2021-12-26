b3b8bb56016d987ce1323c4f71ea4112ce11858a
Jaleel-VS <jeremikress@gmail.com>
2021-12-26 06:48:01 +0200

    update requirements

---
+++ base_cog.py
@@ -0,0 +1,12 @@
+from discord.ext.commands import Cog
+
+
+class BaseCog(Cog):
+    """Base class for all cogs"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @staticmethod
+    async def cog_command_error(ctx, error):
+        print('An error occurred: {}'.format(str(error)))
